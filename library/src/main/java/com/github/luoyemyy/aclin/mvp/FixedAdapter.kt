f5851695a4f927be2bfd17854300f5cabfad4860
yeyuming <luoyemyy@qq.com>
2020-01-03 17:49:40 +0800

    1

--- library/src/main/java/com/github/luoyemyy/aclin/mvp/FixedAdapter.kt
+++ library/src/main/java/com/github/luoyemyy/aclin/mvp/FixedAdapter.kt
@@ -1,10 +1,15 @@
 package com.github.luoyemyy.aclin.mvp
 
+import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
 
 abstract class FixedAdapter<T, BIND : ViewDataBinding>(owner: LifecycleOwner, liveData: ListLiveData<T>) : MvpAdapter<T, BIND>(owner, liveData) {
     init {
         enableMore = false
+    }
+
+    override fun getItemClickViews(binding: BIND): List<View> {
+        return listOf(binding.root)
     }
 }
