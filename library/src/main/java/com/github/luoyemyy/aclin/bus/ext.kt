f11567a282e77b77fd1fb490447fd56f77c82736
yeyuming <luoyemyy@qq.com>
2020-01-08 11:01:21 +0800

    1

--- library/src/main/java/com/github/luoyemyy/aclin/bus/ext.kt
+++ library/src/main/java/com/github/luoyemyy/aclin/bus/ext.kt
@@ -2,8 +2,23 @@ package com.github.luoyemyy.aclin.bus
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 
+
+fun FragmentActivity.setBus(busResult: BusResult,vararg events: String){
+    getBusLiveData().also {
+        it.observe(this, BusObserver(busResult, events.toList()))
+    }
+}
+
+fun FragmentActivity.postBus(event: String, intValue: Int = 0, longValue: Long = 0L, boolValue: Boolean = false, stringValue: String? = null, extra: Bundle? = null) {
+    getBusLiveData().postValue(BusMsg(event, intValue, longValue, boolValue, stringValue, extra))
+}
+
+fun FragmentActivity.getBusLiveData(): BusLiveData {
+    return ViewModelProvider(this)[BusPresenter::class.java].busLiveData
+}
 
 fun Fragment.setBus(busResult: BusResult,vararg events: String) {
     getBusLiveData().also {
